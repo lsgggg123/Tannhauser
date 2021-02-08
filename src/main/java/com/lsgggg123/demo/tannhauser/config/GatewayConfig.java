71eebeae9f5c544b444500642f5385da8835bcdf
萧易 <ls207208@antfin.com>
2021-02-08 19:47:40 +0800

    spring cloud gateway demo

---
+++ src/main/java/com/lsgggg123/demo/tannhauser/config/GatewayConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Alipay.com Inc. Copyright (c) 2004-2021 All Rights Reserved.
+ */
+package com.lsgggg123.demo.tannhauser.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author ls
+ * @version : GatewayConfig.java, v 0.1 2021年02月08日 7:44 下午 ls Exp $
+ */
+@Configuration
+public class GatewayConfig {
+    
+}
