376342a557f441da4ada425882ce55686cb0da0c
萧易 <ls207208@antfin.com>
2023-04-12 16:19:48 +0800

    proxy

--- src/main/java/com/lsgggg123/demo/tannhauser/filter/MyGlobalFilter.java
+++ src/main/java/com/lsgggg123/demo/tannhauser/filter/MyGlobalFilter.java
@@ -11,6 +11,7 @@ import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -33,6 +34,8 @@ public class MyGlobalFilter implements GlobalFilter {
     
     private static final HttpMessageEncoder<Object> HTTP_MESSAGE_ENCODER = new Jackson2JsonEncoder();
     
+    static WebClient WEB_CLIENT = WebClient.create().mutate().build();
+    
     @Override
     @SuppressWarnings("unchecked")
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -43,11 +46,22 @@ public class MyGlobalFilter implements GlobalFilter {
         if (path.startsWith("/163") || path.startsWith("/b")) {
             return chain.filter(exchange);
         } else {
-           return response.writeWith(HTTP_MESSAGE_ENCODER.encode(Mono.just(RET_MAP),
-                    response.bufferFactory(),
-                    ResolvableType.forClass(Map.class),
-                    MediaType.APPLICATION_JSON,
-                    Hints.from(Hints.LOG_PREFIX_HINT, exchange.getLogPrefix())));
+            return WEB_CLIENT
+                    .get()
+                    .uri("http://127.0.0.1:8000/index")
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .map(s -> {
+                        log.info("map: {}");
+                        return s;
+                    }).onErrorResume(t -> Mono.just("error")).doOnError(Throwable::printStackTrace)
+                    .flatMap(s -> {
+                        Map<String, Object> m = new HashMap<>();
+                        m.putAll(RET_MAP);
+                        m.put("html", s);
+                        return response.writeWith(HTTP_MESSAGE_ENCODER.encode(Mono.just(m), response.bufferFactory(), ResolvableType.forClass(Map.class), MediaType.APPLICATION_JSON, Hints.from(Hints.LOG_PREFIX_HINT, exchange.getLogPrefix())));  
+                    });
+           
         }
     }
 }
