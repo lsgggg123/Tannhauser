9d955c0c5be749ef6693b8cfcac02cd5f1a33613
萧易 <ls207208@antfin.com>
2023-04-13 11:47:51 +0800

    proxy

--- src/main/java/com/lsgggg123/demo/tannhauser/filter/MyGlobalFilter.java
+++ src/main/java/com/lsgggg123/demo/tannhauser/filter/MyGlobalFilter.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.ResolvableType;
 import org.springframework.core.codec.Hints;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.HttpMessageEncoder;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
@@ -13,6 +15,8 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.CoreSubscriber;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
@@ -42,26 +46,27 @@ public class MyGlobalFilter implements GlobalFilter {
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
         
+        // 网关的原生支持的 uri 转发
         String path = request.getPath().toString();
         if (path.startsWith("/163") || path.startsWith("/b")) {
             return chain.filter(exchange);
-        } else {
-            return WEB_CLIENT
-                    .get()
-                    .uri("http://127.0.0.1:8000/index")
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .map(s -> {
-                        log.info("map: {}");
-                        return s;
-                    }).onErrorResume(t -> Mono.just("error")).doOnError(Throwable::printStackTrace)
-                    .flatMap(s -> {
-                        Map<String, Object> m = new HashMap<>();
-                        m.putAll(RET_MAP);
-                        m.put("html", s);
-                        return response.writeWith(HTTP_MESSAGE_ENCODER.encode(Mono.just(m), response.bufferFactory(), ResolvableType.forClass(Map.class), MediaType.APPLICATION_JSON, Hints.from(Hints.LOG_PREFIX_HINT, exchange.getLogPrefix())));  
-                    });
-           
         }
+        
+        // 网关做代理, 直接转发响应第三方的内容
+        if (path.startsWith("/proxy")) {
+            return WEB_CLIENT.get().uri("http://127.0.0.1:8000/index").retrieve().bodyToMono(String.class).map(responseBody -> {
+                log.info("map: {}", responseBody);
+                return responseBody;
+            }).doOnError(Throwable::printStackTrace).onErrorResume(t -> Mono.just("error")).flatMap(responseBody -> {
+                response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE);
+                DataBuffer buffer = response.bufferFactory().wrap(responseBody.getBytes());
+                return response.writeWith(Flux.just(buffer));
+            });
+        }
+        
+        // 兜底的 json
+        return response.writeWith(HTTP_MESSAGE_ENCODER.encode(Mono.just(RET_MAP), response.bufferFactory(),
+                    ResolvableType.forClass(Map.class), MediaType.APPLICATION_JSON,
+                    Hints.from(Hints.LOG_PREFIX_HINT, exchange.getLogPrefix())));
     }
 }
